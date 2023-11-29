5ddfe35be732e542830eddae25c88f00b9ed8865
MinhAn <voongocminhan20072002@gmail.com>
2023-11-30 02:01:35 +0700

    Add register and UI login

--- src/main/java/vn/edu/iuh/fit/lab06/controller/UserController.java
+++ src/main/java/vn/edu/iuh/fit/lab06/controller/UserController.java
@@ -1,8 +1,11 @@
 package vn.edu.iuh.fit.lab06.controller;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +13,18 @@ import vn.edu.iuh.fit.lab06.models.User;
 import vn.edu.iuh.fit.lab06.repositories.UserRepository;
 import vn.edu.iuh.fit.lab06.service.UserServices;
 
+import java.time.Instant;
+
 @Controller
 @RequestMapping("/users")
 @NoArgsConstructor @AllArgsConstructor
 public class UserController {
+    @Autowired
     private UserRepository userRepository;
+    @Autowired
     private UserServices userServices;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @GetMapping("/{id}")
     public String getUser(@PathVariable("id") String id){
         return "";
@@ -41,5 +49,21 @@ public class UserController {
     public String login(@ModelAttribute User user, Model model){
         User us = userServices.login(user.getEmail(), user.getPasswordHash());
         return "index";
+    }
+    @GetMapping("/register-form")
+    public String showRegistrationForm(Model model ) {
+        User user = new User();
+        model.addAttribute("user",user);
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user, Model model,@RequestParam("password") String password) {
+        user.setRegisteredAt(Instant.now());
+        System.out.println(password);
+        user.setPasswordHash(passwordEncoder.encode(password));
+        userRepository.save(user);
+        model.addAttribute("message", "Registration successful. Please login.");
+        return "user/login";
     }
 }
